a0e494cc00662c4e1754a9c9e2b12be206f54c85
kasumi8pon <all.splashing@gmail.com>
2018-08-23 20:45:54 +0900

    add id digit, bug fix

--- myapp.rb
+++ myapp.rb
@@ -26,9 +26,9 @@ end
 
 
 get "/" do
-  filenames = Dir.glob("memo/*").reverse
-  @id = filenames.map {|f| File.basename(f, ".txt")}
-  @title =
+  filenames = Dir.glob("memo/*").sort.reverse
+  @ids = filenames.map {|f| File.basename(f, ".txt")}
+  @titles =
   filenames.map do |f|
     title = File.open(f).gets
     title ? title : "(no title)"
@@ -48,7 +48,14 @@ end
 
 post "/create" do
   @body = params[:body]
-  @id = DateTime.now.strftime("%Y%m%d%H%M%S")
+  ids = Dir.glob("memo/*").map {|f| File.basename(f, ".txt")}
+  datetime = DateTime.now.strftime("%Y%m%d%H%M%S")
+  @id = datetime
+  digit = 0
+  while ids.include?(@id)
+    digit += 1
+    @id = datetime + digit.to_s
+  end
   File.write("memo/#{@id}.txt", @body)
   redirect '/'
 end
